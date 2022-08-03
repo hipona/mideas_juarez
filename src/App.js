bd1c23b0e4c71f779871e6a0d2d5289dbf595faf
hipona <mecjuarez@gmail.com>
2022-08-02 22:50:34 -0300

    Martes 23hs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hola
+          Edit <code>src/App.js</code> Nada para ver
         </p>
         
         <a
