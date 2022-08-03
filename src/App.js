b0bfd4cdb62d1daf52c3befc2df1329f508dab32
hipona <mecjuarez@gmail.com>
2022-08-02 22:19:15 -0300

    Nueva Rama

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-let array1 = [1,2,3,4,5,6,7];
-let array2 = [8,9,10];
-let array3 = [...array1, ...array2];
-
-
-console.log(array3)
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Hola
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
